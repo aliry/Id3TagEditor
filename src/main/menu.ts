459c8b00c66154da22a169472b1277e347cc23d8
Ali.Dev <aliry@live.com>
2022-10-29 21:52:59 -0700

    update package assets

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -57,14 +57,14 @@ export default class MenuBuilder {
       label: 'Electron',
       submenu: [
         {
-          label: 'About ElectronReact',
+          label: 'About Music Tag Editor',
           selector: 'orderFrontStandardAboutPanel:',
         },
         { type: 'separator' },
         { label: 'Services', submenu: [] },
         { type: 'separator' },
         {
-          label: 'Hide ElectronReact',
+          label: 'Hide Music Tag Editor',
           accelerator: 'Command+H',
           selector: 'hide:',
         },
